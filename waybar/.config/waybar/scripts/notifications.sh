87dc1e565c5da138a1c36bbdb6750d3826e3f111
Saurabh Kumar <akgec.saurabh@gmail.com>
2025-06-06 01:50:47 +0530

    deleted unused packages config

--- waybar/.config/waybar/scripts/notifications.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if dunstctl is-paused | grep -q "true"; then
-    echo '{"text": "", "class": "dnd", "alt": "dnd"}'
-elif [ "$(dunstctl count waiting)" -gt 0 ]; then
-    echo '{"text": "", "class": "notification", "alt": "notification"}'
-else
-    echo '{"text": "", "class": "none", "alt": "none"}'
-fi
