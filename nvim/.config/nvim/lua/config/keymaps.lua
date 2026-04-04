f155d5886a4828f4c9196c8cc01e2dc9fc47d3ea
Saurabh <akgec.saurabh@gmail.com>
2026-04-04 23:03:05 +0530

    keymaps mini-nvim for buffer

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,9 +1,7 @@
 -- ============================
---
 -- KEYMAPS FILE (keymaps.lua)
 -- ============================
 
--- Leader key
 vim.g.mapleader = " "
 
 local keymap = vim.keymap
@@ -13,19 +11,11 @@ local opts = { noremap = true, silent = true }
 -- BASIC
 -- ============================
 
--- Exit insert mode
 keymap.set("i", "jk", "<Esc>", opts)
 
--- Save file
 keymap.set("n", "<leader>w", "<cmd>w<CR>", opts)
-
--- Quit window
 keymap.set("n", "<leader>q", "<cmd>q<CR>", opts)
-
--- Save and quit
 keymap.set("n", "<leader>wq", "<cmd>wq<CR>", opts)
-
--- Clear search highlight
 keymap.set("n", "<leader>nh", "<cmd>nohlsearch<CR>", opts)
 
 -- ============================
@@ -55,8 +45,16 @@ keymap.set("n", "<S-Tab>", "<cmd>bprevious<CR>", opts)
 
 keymap.set("n", "<leader>bn", "<cmd>bnext<CR>", opts)
 keymap.set("n", "<leader>bp", "<cmd>bprevious<CR>", opts)
-keymap.set("n", "<leader>bd", "<cmd>bdelete<CR>", opts)
-keymap.set("n", "<leader>bo", "<cmd>%bd|e#|bd#<CR>", opts) -- close all except current
+
+keymap.set("n", "<leader>bd", function()
+  require("mini.bufremove").delete(0, false)
+end, { desc = "Delete buffer" })
+
+keymap.set("n", "<leader>bD", function()
+  require("mini.bufremove").delete(0, true)
+end, { desc = "Force delete buffer" })
+
+keymap.set("n", "<leader>bo", "<cmd>%bd|e#|bd#<CR>", opts)
 
 -- ============================
 -- NVIM TREE
@@ -92,7 +90,7 @@ keymap.set("n", "gr", vim.lsp.buf.references, opts)
 keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
 keymap.set("n", "<leader>lf", function()
-    require("conform").format({ async = true, lsp_fallback = true })
+  require("conform").format({ async = true, lsp_fallback = true })
 end, opts)
 
 keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
@@ -103,11 +101,9 @@ keymap.set("n", "<leader>ds", vim.diagnostic.open_float, opts)
 -- VISUAL MODE
 -- ============================
 
--- Better indenting in visual mode
 keymap.set("v", "<", "<gv", opts)
 keymap.set("v", ">", ">gv", opts)
 
--- Move selected lines up/down
 keymap.set("v", "J", ":m '>+1<CR>gv=gv", opts)
 keymap.set("v", "K", ":m '<-2<CR>gv=gv", opts)
 
@@ -115,5 +111,4 @@ keymap.set("v", "K", ":m '<-2<CR>gv=gv", opts)
 -- QUICK
 -- ============================
 
--- Select all
 keymap.set("n", "<leader>a", "ggVG", opts)
